7c359f51ab40cc7123d3552d809b816f387f5950
unknown <edwinrmrjr@gmail.com>
2024-10-15 14:15:40 +0800

    part4 Online Tracker with Websockets

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -15,12 +15,24 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name, self.channel_name
         )
 
+        # add and update online users
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+
         self.accept()
+
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name, self.channel_name
         )
+
+        # remove and update online users
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
+
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -41,6 +53,7 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name, event
         )
 
+
     def message_handler(self, event):
         message_id = event['message_id']
         message = GroupMessage.objects.get(id=message_id)
@@ -50,4 +63,30 @@ class ChatroomConsumer(WebsocketConsumer):
             'chat_group': self.chatroom
         }
         html = render_to_string("a_rtchat/partials/chat_message_p.html", context=context)
+        self.send(text_data=html)
+
+
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count() -1
+
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+
+
+    def online_count_handler(self, event):
+        online_count = event['online_count']
+
+        chat_messages = ChatGroup.objects.get(group_name=self.chatroom_name).chat_messages.all()[:30]
+        author_ids = set([message.author.id for message in chat_messages])
+        users = User.objects.filter(id__in=author_ids)
+
+        context = {
+            'online_count' : online_count,
+            'chat_group' : self.chatroom,
+            'users': users
+        }
+        html = render_to_string("a_rtchat/partials/online_count.html", context)
         self.send(text_data=html)
