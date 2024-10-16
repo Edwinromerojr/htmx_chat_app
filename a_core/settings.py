668598ed2bb98c87bca1ef269e60de31cba18738
unknown <edwinrmrjr@gmail.com>
2024-10-16 09:40:30 +0800

    part6 Private Chat

--- a_core/settings.py
+++ a_core/settings.py
@@ -91,20 +91,20 @@ WSGI_APPLICATION = 'a_core.wsgi.application'
 ASGI_APPLICATION = 'a_core.asgi.application'
 
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels.layers.InMemoryChannelLayer"
-#     }
-# }
-
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis://127.0.0.1:6379")],
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("redis://127.0.0.1:6379")],
+#         },
+#     },
+# }
 
 
 # Database
